34e091dc5a8e10995ce7b6c25940c1ca28d61d9a
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-10-02 22:39:12 +0700

    add morgan logging

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import {
 } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('tiny'));
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
