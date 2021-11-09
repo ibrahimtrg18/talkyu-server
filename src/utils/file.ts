c4bf532b90befc43053195122c9cfedbe7321996
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-09 13:13:32 +0700

    improving getFileToBase64 with format

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -5,11 +5,12 @@ import * as fileType from 'file-type';
 interface Path {
   prefix: string[];
   name: string;
+  mimetype?: string;
   ext?: string;
 }
 
 export const createFile = async (base64: string, { prefix, name }: Path) => {
-  const cleanBase64 = base64.replace(/^data:image\/png;base64,/, '');
+  const cleanBase64 = base64.split(',')[1];
   const buffer = Buffer.from(cleanBase64, 'base64');
   const file = await fileType.fromBuffer(buffer);
   return fs.writeFileSync(
@@ -23,14 +24,27 @@ export const createFile = async (base64: string, { prefix, name }: Path) => {
 
 export const getFile = async ({ prefix, name, ext }: Path) => {
   return fs.createReadStream(
-    path.join(path.resolve('./'), ...['public', ...prefix, `${name}.${ext}`]),
+    path.join(
+      path.resolve('./'),
+      ...['public', ...prefix, ext ? `${name}.${ext}` : name],
+    ),
   );
 };
 
-export const getFileToBase64 = ({ prefix, name, ext }: Path) => {
-  return fs
-    .readFileSync(
-      path.join(path.resolve('./'), ...['public', ...prefix, `${name}.${ext}`]),
-    )
-    .toString('base64');
+export const getFileToBase64 = ({ prefix, name, ext, mimetype = '' }: Path) => {
+  return formatBase64({
+    mimetype,
+    encoded: fs
+      .readFileSync(
+        path.join(
+          path.resolve('./'),
+          ...['public', ...prefix, ext ? `${name}.${ext}` : name],
+        ),
+      )
+      .toString('base64'),
+  });
+};
+
+const formatBase64 = ({ mimetype, charset = 'base64', encoded }) => {
+  return `data:${mimetype};${charset},${encoded}`;
 };
