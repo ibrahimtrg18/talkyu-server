8748a385cb8840721258cfee63cd2d99ff5b1771
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2022-06-04 20:58:32 +0700

    refactoring: initial repository name

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -12,18 +12,18 @@ import { Post } from './entities/post.entity';
 export class PostService {
   constructor(
     @InjectRepository(Post)
-    private readonly postRespository: Repository<Post>,
+    private readonly postRepository: Repository<Post>,
     @InjectRepository(User)
-    private readonly userRespository: Repository<User>,
+    private readonly userRepository: Repository<User>,
     @InjectRepository(Comment)
-    private readonly commentRespository: Repository<Comment>,
+    private readonly commentRepository: Repository<Comment>,
   ) {}
 
   async create(createPostDto: CreatePostDto) {
     try {
-      const user = await this.userRespository.findOne(createPostDto.user.id);
+      const user = await this.userRepository.findOne(createPostDto.user.id);
 
-      const newPost = await this.postRespository.save({
+      const newPost = await this.postRepository.save({
         ...createPostDto,
         user,
       });
@@ -37,7 +37,7 @@ export class PostService {
 
   async findById(id: string) {
     try {
-      const post = await this.postRespository
+      const post = await this.postRepository
         .createQueryBuilder('post')
         .leftJoinAndSelect('post.like_by_users', 'postLikes')
         .leftJoinAndSelect('post.comments', 'comments')
@@ -54,7 +54,7 @@ export class PostService {
   async isLikedById(id: string) {
     const post = await this.findById(id);
 
-    const isLiked = await this.userRespository.findByIds(post.like_by_users);
+    const isLiked = await this.userRepository.findByIds(post.like_by_users);
 
     return isLiked;
   }
@@ -63,9 +63,9 @@ export class PostService {
     try {
       const post = await this.findById(id);
 
-      const user = await this.userRespository.findOne(userId);
+      const user = await this.userRepository.findOne(userId);
 
-      const likedPost = await this.postRespository.save({
+      const likedPost = await this.postRepository.save({
         ...post,
         like_by_users: [...post.like_by_users, user],
       });
@@ -83,22 +83,17 @@ export class PostService {
     userId: string,
   ) {
     try {
-      const user = await this.userRespository.findOne(userId);
+      const user = await this.userRepository.findOne(userId);
 
-      const post = await this.findById(id);
+      const post = await this.postRepository.findOne(id);
 
-      const comment = await this.commentRespository.save({
+      const comment = await this.commentRepository.save({
         ...createCommentDto,
         user,
         post,
       });
 
-      const commentedPost = await this.postRespository.save({
-        ...post,
-        comments: [...post.comments, comment],
-      });
-
-      return commentedPost;
+      return comment;
     } catch (error) {
       console.error(error);
       throw new Error(error);
@@ -107,7 +102,7 @@ export class PostService {
 
   async remove(postId: string) {
     try {
-      const deletedPost = await this.postRespository.delete({ id: postId });
+      const deletedPost = await this.postRepository.delete({ id: postId });
 
       return deletedPost;
     } catch (error) {
